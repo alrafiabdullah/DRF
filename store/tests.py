96a3d6d6332752fd78d1b6b592472c1b8b666481
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2020-09-18 05:30:03 +0600

    POST request test added

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
+from .models import Product
 # Create your tests here.
+
+
+class ProductCreateTestCase(APITestCase):
+    def test_create_product(self):
+        initial_product_count = Product.objects.count()
+        product_attrs = {
+            'name': 'New Product',
+            'description': 'Awesome product.',
+            'price': 99.99,
+        }
+        response = self.client.post(
+            '/api/v1/products/new', product_attrs, format='json')
+        if response.status_code != 201:
+            print(response.data)
+        self.assertEqual(
+            Product.objects.count(),
+            initial_product_count + 1,
+        )
+        for attr, expected_value in product_attrs.items():
+            self.assertEqual(response.data[attr], expected_value)
