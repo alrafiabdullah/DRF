2f2d242c0406dfbf90e60e69221c7df83c8b477d
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2020-09-18 14:27:20 +0600

    imagefield added

--- store/models.py
+++ store/models.py
@@ -7,5 +7,6 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=999)
     price = models.FloatField()
+    photo = models.ImageField(null=True, blank=True, upload_to='images/')
     sale_start = models.DateTimeField(blank=True, null=True)
     sale_end = models.DateTimeField(blank=True, null=True)
