61bf849209bab24649aa20d20987c589f6489cca
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2020-09-18 13:52:50 +0600

    DELETE request test added

--- store/tests.py
+++ store/tests.py
@@ -22,3 +22,29 @@ class ProductCreateTestCase(APITestCase):
         )
         for attr, expected_value in product_attrs.items():
             self.assertEqual(response.data[attr], expected_value)
+
+
+class ProductDestroyTestCase(APITestCase):
+    def test_delete_product(self):
+
+        product_attrs = {
+            'name': 'New Product',
+            'description': 'Awesome product.',
+            'price': 99.99,
+        }
+        response = self.client.post(
+            '/api/v1/products/new', product_attrs, format='json')
+        if response.status_code != 201:
+            print(response.data)
+        initial_product_count = Product.objects.count()
+        product_id = Product.objects.first().id
+
+        self.client.delete('/api/v1/products/{}/update'.format(product_id))
+        self.assertEqual(
+            Product.objects.count(),
+            initial_product_count - 1,
+        )
+        self.assertRaises(
+            Product.DoesNotExist,
+            Product.objects.get, id=product_id,
+        )
